6f75daeca951e63fe797b6b3ba27cf48cb9b84d0
zlguo <zlguo3937@gmail.com>
2024-07-24 18:55:56 +0800

    update script csv2yml 20%

--- script/csv2yml/ana_csv2yaml.py
+++ script/csv2yml/ana_csv2yaml.py
@@ -6,6 +6,10 @@ import yaml
 class RegisterParser:
     def __init__(self):
         self.regs_type_list = ['ID', 'OD', 'IDH', 'ODH', 'OCK', 'PIOA', 'PIA', 'POA', 'IA', 'OA']
+        self.apb_type_list = ['ro', 'rw', 'raw', 'wo', 'rc', 'sc', 'rwc', 'lh', 'll']
+        self.cpu_type_list = ['ro', 'rw', 'raw', 'wo', 'rc', 'sc', 'rwc', 'lh', 'll']
+        self.mdio_type_list = ['ro', 'rw', 'raw', 'wo', 'rc', 'sc', 'rwc', 'lh', 'll']
+        self.dev_type_list = ['ro', 'rw', 'raw', 'wo', 'we', 'none']
 
     def parse_bit_range(self, bit_range):
         reg_range = bit_range.strip()
@@ -16,8 +20,8 @@ class RegisterParser:
             msb = lsb = int(int_range)
         return msb, lsb
 
-    def parse_reg_init(self, reset, msb, lsb):
-        init = int(reset.strip(), 16)
+    def parse_reg_init(self, init, msb, lsb):
+        init = int(init.strip(), 16)
         if init == '':
             print("WARNING: Empty Default Value!")
             sys.exit(-1)
@@ -26,6 +30,78 @@ class RegisterParser:
                 msb - lsb + 1, (2 ** (msb - lsb + 1)), init))
             sys.exit(-1)
         return init
+
+    def parse_apb_tpe(self, regs_type):
+        apb_type = self.parse_reg_type(regs_type)
+        if apb_type in ['ROR', 'ROD', 'RODEV', 'RORSYNC', 'RODSYNC', 'RODEVSYNC', 'CMRO']:
+            apb_type = 'ro'
+        elif apb_type in ['RWR', 'RWD', 'RWDEV', 'RWRSYNC', 'RWDSYNC', 'RWDEVSYNC', 'CMRW', 'MCRO', 'MCRC', 'INC_CNT', 'RAW']:
+            apb_type = 'rw'
+        if apb_type not in self.apb_type_list:
+            print("ERROR: Your Register type must be in apb_type_list!")
+            print(apb_type)
+            sys.exit(-1)
+        return apb_type
+
+    def parse_cpu_tpe(self, regs_type):
+        cpu_type = self.parse_reg_type(regs_type)
+        if cpu_type in ['ROR', 'ROD', 'RODEV', 'RORSYNC', 'RODSYNC', 'RODEVSYNC', 'MCRO']:
+            cpu_type = 'ro'
+        elif cpu_type in ['RWR', 'RWD', 'RWDEV', 'RWRSYNC', 'RWDSYNC', 'RWDEVSYNC', 'CMRW', 'CMRO', 'LHRC', 'INC_CNT', 'RAW']:
+            cpu_type = 'rw'
+        elif cpu_type in ['MCRC']:
+            cpu_type = 'rc'
+        elif cpu_type in ['SC', 'CMRC']:
+            cpu_type = 'sc'
+        elif cpu_type in ['RC', 'RCSYNC']:
+            cpu_type = 'rc'
+        elif cpu_type in ['ROLH', 'CPUROLH']:
+            cpu_type = 'lh'
+        elif cpu_type in ['ROLL', 'CPUROLL']:
+            cpu_type = 'll'
+        if cpu_type not in self.cpu_type_list:
+            print("ERROR: Your Register type must be in cpu_type_list!")
+            print(cpu_type)
+            sys.exit(-1)
+        return cpu_type
+
+    def parse_mdio_tpe(self, regs_type):
+        mdio_type = self.parse_reg_type(regs_type)
+        if mdio_type in ['ROR', 'ROD', 'RODEV', 'RORSYNC', 'RODSYNC', 'RODEVSYNC', 'MCRO']:
+            mdio_type = 'ro'
+        elif mdio_type in ['RWR', 'RWD', 'RWDEV', 'RWRSYNC', 'RWDSYNC', 'RWDEVSYNC', 'CMRW', 'CMRO', 'LHRC', 'INC_CNT', 'RAW']:
+            mdio_type = 'rw'
+        elif mdio_type in ['MCRC']:
+            mdio_type = 'rc'
+        elif mdio_type in ['SC', 'CMRC']:
+            mdio_type = 'sc'
+        elif mdio_type in ['RC', 'RCSYNC']:
+            mdio_type = 'rc'
+        elif mdio_type in ['ROLH', 'CPUROLH']:
+            mdio_type = 'lh'
+        elif mdio_type in ['ROLL', 'CPUROLL']:
+            mdio_type = 'll'
+        if mdio_type not in self.mdio_type_list:
+            print("ERROR: Your Register type must be in mdio_type_list!")
+            print(mdio_type)
+            sys.exit(-1)
+        return mdio_type
+
+    def parse_dev_tpe(self, regs_type):
+        dev_type = self.parse_reg_type(regs_type)
+        if dev_type in ['ROR', 'ROD', 'RORSYNC', 'RODSYNC', 'ROLH', 'ROLL']:
+            dev_type = 'wo'
+        elif dev_type in ['RODEV', 'RWDEV', 'RODEVSYNC', 'RWDEVSYNC', 'RC', 'RCSYNC']:
+            dev_type = 'we'
+        elif dev_type in ['RWR', 'RWD', 'RWRSYNC', 'RWDSYNC', 'SC']:
+            dev_type = 'ro'
+        else:
+            dev_type = 'none'
+        if dev_type not in self.dev_type_list:
+            print("ERROR: Your Register type must be in dev_type_list!")
+            print(dev_type)
+            sys.exit(-1)
+        return dev_type
 
     def parse_reg_type(self, regs_type):
         if regs_type != '' and regs_type not in self.regs_type_list:
@@ -53,7 +129,7 @@ class RegisterParser:
 
     def read_csv_to_yaml(self, csv_file):
         address = 0
-        reg_group_name: str = ""
+        group_name: str = ""
         desc: str = ""
 
         addresses = set()
@@ -69,13 +145,13 @@ class RegisterParser:
                     continue
 
                 if row[0].startswith("##"):
-                    reg_group_name = row[0][2:]
-                    if reg_group_name != '' and reg_group_name in reg_group_names:
-                        print("ERROR: Your Register reg_group_name must be exclusive in all regs!")
-                        print(reg_group_name)
+                    group_name = row[0][2:]
+                    if group_name != '' and group_name in reg_group_names:
+                        print("ERROR: Your Register group_name must be exclusive in all regs!")
+                        print(group_name)
                         sys.exit(-1)
                     else:
-                        reg_group_names.add(reg_group_name)
+                        reg_group_names.add(group_name)
                     desc = row[0][2:]
 
                     if address in addresses:
@@ -86,9 +162,9 @@ class RegisterParser:
                         addresses.add(address)
                     reg_info = {
                         'address': hex(address),
-                        'reg_group_name': reg_group_name,
+                        'group_name': group_name,
                         'desc': desc,
-                        'register': [],
+                        'fields': [],
                         'ana_io': []
                     }
                     address = address + 1
@@ -96,20 +172,26 @@ class RegisterParser:
                 elif row[0].startswith("["):
                     msb, lsb = self.parse_bit_range(row[0])
                     width = msb - lsb + 1
+                    type_reg = {}
+                    type_reg['apb'] = self.parse_apb_tpe(row[3].strip())
+                    type_reg['cpu'] = self.parse_cpu_tpe(row[3].strip())
+                    type_reg['mdio'] = self.parse_mdio_tpe(row[3].strip())
+                    type_reg['dev'] = self.parse_dev_tpe(row[3].strip())
                     field_info = {
                         'name': self.parse_reg_name(row[1].strip(), all_regs),
                         'type': self.parse_reg_type(row[3].strip()),
-                        'range': width,
+                        'range': row[0],
                         'msb': msb,
                         'lsb': lsb,
                         'width': width,
-                        'reset': hex(self.parse_reg_init(row[5], msb, lsb)),
+                        'init': hex(self.parse_reg_init(row[5], msb, lsb)),
+                        'field_type': type_reg,
                         'dc_scan_mode': row[6],
                         'disable_scan': row[7],
                         'occ_scan_mode': row[8],
                         'special_dft_control': row[9],
                     }
-                    reg_info['register'].append(field_info)
+                    reg_info['fields'].append(field_info)
                     all_regs.append(row[1].strip())
                 elif row[0].strip() == '':
                     ana_io_info = {
--- script/csv2yml/csv_2_yaml.py
+++ script/csv2yml/csv_2_yaml.py
@@ -11,6 +11,10 @@ class RegisterParser:
                                'ROLH', 'ROLL', 'CMRW', 'CMRO', 'CMRC',
                                'MCRO', 'MCRC', 'LHRC', 'CPUROLH', 'CPUROLL',
                                'INC_CNT', 'RCSYNC', 'RAW']
+        self.apb_type_list = ['ro', 'rw', 'raw', 'wo', 'rc', 'sc', 'rwc', 'lh', 'll']
+        self.cpu_type_list = ['ro', 'rw', 'raw', 'wo', 'rc', 'sc', 'rwc', 'lh', 'll']
+        self.mdio_type_list = ['ro', 'rw', 'raw', 'wo', 'rc', 'sc', 'rwc', 'lh', 'll']
+        self.dev_type_list = ['ro', 'rw', 'raw', 'wo', 'we', 'none']
 
     def parse_bit_range(self, bit_range):
         reg_range = bit_range.strip()
@@ -39,6 +43,88 @@ class RegisterParser:
             sys.exit(-1)
         return regs_type
 
+    def parse_apb_tpe(self, regs_type):
+        apb_type = self.parse_reg_type(regs_type)
+        if apb_type in ['ROR', 'ROD', 'RODEV', 'RORSYNC', 'RODSYNC', 'RODEVSYNC', 'CMRO']:
+            apb_type = 'ro'
+        elif apb_type in ['RWR', 'RWD', 'RWDEV', 'RWRSYNC', 'RWDSYNC', 'RWDEVSYNC', 'CMRW', 'MCRO', 'MCRC', 'INC_CNT', 'RAW']:
+            apb_type = 'rw'
+        elif apb_type in ['CMRC']:
+            apb_type = 'rc'
+        elif apb_type in ['SC', 'CMRC']:
+            apb_type = 'sc'
+        elif apb_type in ['RC', 'RCSYNC']:
+            apb_type = 'rc'
+        elif apb_type in ['ROLH', 'CPUROLH']:
+            apb_type = 'lh'
+        elif apb_type in ['ROLL', 'CPUROLL']:
+            apb_type = 'll'
+        if apb_type not in self.apb_type_list:
+            print("ERROR: Your Register type must be in apb_type_list!")
+            print(apb_type)
+            sys.exit(-1)
+        return apb_type
+
+    def parse_cpu_tpe(self, regs_type):
+        cpu_type = self.parse_reg_type(regs_type)
+        if cpu_type in ['ROR', 'ROD', 'RODEV', 'RORSYNC', 'RODSYNC', 'RODEVSYNC', 'MCRO']:
+            cpu_type = 'ro'
+        elif cpu_type in ['RWR', 'RWD', 'RWDEV', 'RWRSYNC', 'RWDSYNC', 'RWDEVSYNC', 'CMRW', 'CMRO', 'LHRC', 'INC_CNT', 'RAW']:
+            cpu_type = 'rw'
+        elif cpu_type in ['MCRC']:
+            cpu_type = 'rc'
+        elif cpu_type in ['SC', 'CMRC']:
+            cpu_type = 'sc'
+        elif cpu_type in ['RC', 'RCSYNC']:
+            cpu_type = 'rc'
+        elif cpu_type in ['ROLH', 'CPUROLH']:
+            cpu_type = 'lh'
+        elif cpu_type in ['ROLL', 'CPUROLL']:
+            cpu_type = 'll'
+        if cpu_type not in self.cpu_type_list:
+            print("ERROR: Your Register type must be in cpu_type_list!")
+            print(cpu_type)
+            sys.exit(-1)
+        return cpu_type
+
+    def parse_mdio_tpe(self, regs_type):
+        mdio_type = self.parse_reg_type(regs_type)
+        if mdio_type in ['ROR', 'ROD', 'RODEV', 'RORSYNC', 'RODSYNC', 'RODEVSYNC', 'MCRO']:
+            mdio_type = 'ro'
+        elif mdio_type in ['RWR', 'RWD', 'RWDEV', 'RWRSYNC', 'RWDSYNC', 'RWDEVSYNC', 'CMRW', 'CMRO', 'LHRC', 'INC_CNT', 'RAW']:
+            mdio_type = 'rw'
+        elif mdio_type in ['MCRC']:
+            mdio_type = 'rc'
+        elif mdio_type in ['SC', 'CMRC']:
+            mdio_type = 'sc'
+        elif mdio_type in ['RC', 'RCSYNC']:
+            mdio_type = 'rc'
+        elif mdio_type in ['ROLH', 'CPUROLH']:
+            mdio_type = 'lh'
+        elif mdio_type in ['ROLL', 'CPUROLL']:
+            mdio_type = 'll'
+        if mdio_type not in self.mdio_type_list:
+            print("ERROR: Your Register type must be in mdio_type_list!")
+            print(mdio_type)
+            sys.exit(-1)
+        return mdio_type
+
+    def parse_dev_tpe(self, regs_type):
+        dev_type = self.parse_reg_type(regs_type)
+        if dev_type in ['ROR', 'ROD', 'RORSYNC', 'RODSYNC', 'ROLH', 'ROLL']:
+            dev_type = 'wo'
+        elif dev_type in ['RODEV', 'RWDEV', 'RODEVSYNC', 'RWDEVSYNC', 'RC', 'RCSYNC']:
+            dev_type = 'we'
+        elif dev_type in ['RWR', 'RWD', 'RWRSYNC', 'RWDSYNC', 'SC']:
+            dev_type = 'ro'
+        else:
+            dev_type = 'none'
+        if dev_type not in self.dev_type_list:
+            print("ERROR: Your Register type must be in dev_type_list!")
+            print(dev_type)
+            sys.exit(-1)
+        return dev_type
+
     def parse_reg_name(self, reg_name, all_regs):
         if reg_name in all_regs:
             print("ERROR: Your Register name must be exclusive in all regs!")
@@ -48,7 +134,7 @@ class RegisterParser:
 
     def read_csv_to_yaml(self, csv_file):
         address: str = ""
-        reg_group_name: str = ""
+        group_name: str = ""
         desc: str = ""
 
         addresses = set()
@@ -64,13 +150,13 @@ class RegisterParser:
                     continue
 
                 if row[0].startswith("#"):
-                    reg_group_name = row[1]
-                    if reg_group_name in reg_group_names:
-                        print("ERROR: Your Register reg_group_name must be exclusive in all regs!")
-                        print(reg_group_name)
+                    group_name = row[1]
+                    if group_name in reg_group_names:
+                        print("ERROR: Your Register group_name must be exclusive in all regs!")
+                        print(group_name)
                         sys.exit(-1)
                     else:
-                        reg_group_names.add(reg_group_name)
+                        reg_group_names.add(group_name)
                     desc = row[2]
                 elif row[0].startswith("") and "addr=" in row[1]:
                     address = hex(int(row[1].strip()[7:], 16))
@@ -82,24 +168,30 @@ class RegisterParser:
                         addresses.add(address)
                     reg_info = {
                         'address': address,
-                        'reg_group_name': reg_group_name,
+                        'group_name': group_name,
                         'desc': desc,
-                        'register': []
+                        'fields': []
                     }
                     regs_info.append(reg_info)
                 elif row[1].startswith("["):
                     msb, lsb = self.parse_bit_range(row[1])
                     width = msb - lsb + 1
+                    type_reg = {}
+                    type_reg['apb'] = self.parse_apb_tpe(row[3].strip())
+                    type_reg['cpu'] = self.parse_cpu_tpe(row[3].strip())
+                    type_reg['mdio'] = self.parse_mdio_tpe(row[3].strip())
+                    type_reg['dev'] = self.parse_dev_tpe(row[3].strip())
                     field_info = {
-                        'name': self.parse_reg_name(row[4].strip(), all_regs),
+                        'range': row[1],
                         'type': self.parse_reg_type(row[3].strip()),
-                        'range': width,
                         'msb': msb,
                         'lsb': lsb,
                         'width': width,
-                        'reset': hex(self.parse_reg_init(row[2], msb, lsb)),
+                        'init': hex(self.parse_reg_init(row[2], msb, lsb)),
+                        'field_type': type_reg,
+                        'name': self.parse_reg_name(row[4].strip(), all_regs),
                     }
-                    reg_info['register'].append(field_info)
+                    reg_info['fields'].append(field_info)
                     all_regs.append(row[4].strip())
 
         return regs_info
